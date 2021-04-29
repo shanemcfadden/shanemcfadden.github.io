65f910d839a1a145266bfd9d4f79fe1cac4d7895
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-04-29 11:01:02 -0500

    Add github and linkedin links

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import "normalize.css";
 import "../styles/index.scss";
 import ContentCard from "../components/ContentCard";
 import { graphql } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const IndexPage = ({ data }) => {
   const projects = data.allMarkdownRemark.nodes;
@@ -15,6 +17,14 @@ const IndexPage = ({ data }) => {
           <h1 className="align-center">Shane McFadden</h1>
           <div className="align-center">and I'm a</div>
           <h2 className="align-right">Full stack developer</h2>
+          <div className="social-links">
+            <a href="https://github.com/shanemcfadden">
+              <FontAwesomeIcon icon={faGithubSquare} />
+            </a>
+            <a href="https://linkedin.com">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </div>
         </div>
       </div>
       <div className="content-container">
