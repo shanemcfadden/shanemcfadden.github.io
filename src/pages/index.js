37df51311d94fc7ec8427ffceb4f796beb035a0f
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-05-07 11:47:23 -0500

    Init "About me"

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,6 +31,45 @@ const IndexPage = ({ data }) => {
         </div>
       </div>
       <div className="content-container">
+        <h2>About me</h2>
+        <p>
+          I moved to Chicago a few years ago with a couple of music degrees in
+          hand and began my work as a professional musician. As a pianist, I
+          accompanied classical singers and instrumentalists at countless
+          recitals, classes, and other events. The bulk of my daily work was as
+          a vocal coach, where I helped vocalists tackle the challenges of
+          performing their music with a compelling interpretation and crystal
+          clear diction in any language they needed to sing.
+        </p>
+        <p>
+          In March of 2020, in-person work shut down completely due to the
+          COVID-19 pandemic, and it was no longer safe to rehearse, perform, or
+          even sing in communal spaces. Besides doing a few accompaniment
+          recordings and online coachings each week, my music work essentially
+          disappeared overnight.
+        </p>
+        <p>
+          However, I decided to use that extra time to my advantage. I had never
+          taken a computer science course in school, but I was always fascinated
+          by the idea of being able to build a webpage or automate a task by
+          writing a few lines of code. Fueled by a rabid curiosity and a love of
+          learning, I took online courses in HTML, CSS, and Javascript on
+          Skillshare and Udemy. That led to more coursework in data structures,
+          algorithms, and back-end technologies. After months of absorbing
+          information from step-by-step classes, I decided to put my skills to
+          the test and begin making my own projects, even as my music work began
+          to pick back up again.
+        </p>
+        <p>
+          Today I'm excited to see where these new skills will take me. It's
+          been incredibly fulfilling to start a project, run into unexpected
+          challenges along the way, and come out on the other side of them even
+          more knowledgeable than before. Because my entire progression thus far
+          has been self-taught, I'm confident in my ability to dig deeper into
+          my existing skills and learn new tech stacks as a member of a team. My
+          objective is to work for a company where I can continue that growth
+          and help develop software at a larger scale.
+        </p>
         <h2>Recent projects</h2>
         {projects.map(({ frontmatter, html }, i) => {
           const { githubRepo, postmanDocs, siteLink } = frontmatter;
