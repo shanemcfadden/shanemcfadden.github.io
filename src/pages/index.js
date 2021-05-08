e1d7e017a56731a7caa61e8e1079fc1f53872f53
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-05-08 16:58:01 -0500

    Align university headings to left

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,13 +98,13 @@ const IndexPage = ({ data }) => {
           <div className="content-card">
             <h1>Education</h1>
             <p>
-              <h3 className="margin-zero">University of Michigan</h3>
+              <h3 className="margin-zero align-left">University of Michigan</h3>
               <div>Master of Music </div>
               <div>Collaborative Piano</div>
               <div>2016-2018</div>
             </p>
             <p>
-              <h3 className="margin-zero">University of Kansas</h3>
+              <h3 className="margin-zero align-left">University of Kansas</h3>
               <div>Bachelor of Music </div>
               <div>Piano </div>
               <div>2012-2016</div>
