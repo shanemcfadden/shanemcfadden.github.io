24c5325c45587acb886a4bd2caffd6de96c14b32
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-05-15 18:06:22 -0500

    Refactor SocialLinks

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -3,24 +3,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const SocialLinks = ({ color }) => {
+  const SOCIAL_LINKS = [
+    {
+      link: "https://github.com/shanemcfadden",
+      icon: faGithubSquare,
+    },
+    {
+      link: "https://linkedin.com",
+      icon: faLinkedin,
+    },
+  ];
   return (
     <div className="social-links">
-      <a href="https://github.com/shanemcfadden" className="social-link">
-        <FontAwesomeIcon
-          className={`social-link__icon${
-            color ? ` social-link__icon-${color}` : ""
-          }`}
-          icon={faGithubSquare}
-        />
-      </a>
-      <a href="https://linkedin.com" className="social-link">
-        <FontAwesomeIcon
-          className={`social-link__icon${
-            color ? ` social-link__icon-${color}` : ""
-          }`}
-          icon={faLinkedin}
-        />
-      </a>
+      {SOCIAL_LINKS.map(({ link, icon }) => (
+        <a href={link} className="social-link">
+          <FontAwesomeIcon
+            className={`social-link__icon${
+              color ? ` social-link__icon-${color}` : ""
+            }`}
+            icon={icon}
+          />
+        </a>
+      ))}
     </div>
   );
 };
