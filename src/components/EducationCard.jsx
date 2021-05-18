aecf654c8aaa6700517233f92e8c9fb4df0dfb39
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-05-18 14:49:10 -0500

    Allow header margin only when flex-direction is column

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const EducationCard = ({ school, degree, major, startYear, endYear }) => {
   return (
     <div className="flex-grow">
-      <h3 className="margin-zero align-left">{degree}</h3>
+      <h3 className="margin-bottom-zero align-left">{degree}</h3>
       <strong>{major}</strong>
       <br />
       {school}
