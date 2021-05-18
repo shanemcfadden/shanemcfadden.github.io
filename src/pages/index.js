3faa00ce788b98cf728f3c52a4b6ff57ae0f5d5e
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-05-18 17:22:54 -0500

    Change full-stack dev to software dev

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,14 +24,14 @@ const IndexPage = ({ data }) => {
     <>
       <main>
         <Helmet>
-          <title>Shane McFadden - Full-Stack Developer</title>
+          <title>Shane McFadden - Software Developer</title>
         </Helmet>
         <div className="banner">
           <div className="content-container">
             <div>Hi, my name is</div>
             <h1 className="align-center">Shane McFadden</h1>
             <div className="align-center">and I'm a</div>
-            <h2 className="align-right">Full-Stack Developer</h2>
+            <h2 className="align-right">Software Developer</h2>
             <SocialLinks socialLinks={SOCIAL_LINKS} />
           </div>
         </div>
