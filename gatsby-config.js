b8d996644b5d8ee446819eb9feb3ce05b29bf7a8
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-05-24 10:15:38 -0500

    Add gatsby-plugin-react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sitemap",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
