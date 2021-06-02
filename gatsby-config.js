1522c8f0bc80f71cb73de810930b6ed924a09ac9
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-06-02 16:10:48 -0500

    Add gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
+    "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sitemap",
