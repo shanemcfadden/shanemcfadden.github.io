320097ff33095fcab744f4aa234ed34dfe1841bb
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-06-02 17:07:33 -0500

    Fix typo

--- src/components/PageCardLayout/PageCardNavigation.jsx
+++ src/components/PageCardLayout/PageCardNavigation.jsx
@@ -8,7 +8,7 @@ const PageCardNavigation = ({ selectedPageIndex, setSelectedPageIndex }) => {
         <h1
           onClick={(e) => {
             e.preventDefault();
-            if (selectedPageIndex != i) setSelectedPageIndex(i);
+            if (selectedPageIndex !== i) setSelectedPageIndex(i);
           }}
           className={i === selectedPageIndex ? "nav__button--selected" : ""}
           key={i}
