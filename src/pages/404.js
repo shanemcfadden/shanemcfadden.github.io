2740be7721a4de6ff98d4c381e00266caecf6d4e
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-06-03 11:09:08 -0500

    navigate to home page using siteMetadata

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,23 @@
 import { useEffect } from "react";
 import { navigate } from "@reach/router";
+import { graphql } from "gatsby";
 
-const NotFoundPage = () => {
+const NotFoundPage = ({ data }) => {
+  const { siteUrl } = data.site.siteMetadata;
   useEffect(() => {
-    navigate("https://shanemcfadden.dev/");
-  }, []);
+    navigate(siteUrl);
+  }, [siteUrl]);
   return null;
 };
 
 export default NotFoundPage;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`;
