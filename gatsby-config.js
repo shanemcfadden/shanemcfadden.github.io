1636ce0fdb3477ea47f827f06360f3f4ae4e2d6f
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-06-02 23:41:49 -0500

    Place site metadata in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: "github-homepage",
-    siteUrl: "https://shanemcfadden.dev",
+    title: "Shane McFadden - Software Developer",
+    description:
+      "Portfolio site for Shane McFadden, a software developer dedicated to building full-stack applications with React, Javascript, Typescript, NodeJS, MySQL, and more.",
+    url: "https://shanemcfadden.dev",
+    twitterImage: "/twitter-image.jpg",
+    ogImage: "/og-image.jpg",
   },
   plugins: [
     "gatsby-plugin-sass",
--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,33 +1,29 @@
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 import Helmet from "react-helmet";
-import PAGE_DESCRIPTION from "../content/PageDescription";
 
 const SEO = () => {
+  const { site } = useStaticQuery(query);
+  const { description, ogImage, title, twitterImage, url } = site.siteMetadata;
   return (
     <Helmet>
       <html lang="en" />
-      <title>Shane McFadden - Software Developer</title>
-      <meta name="description" content={PAGE_DESCRIPTION} />
-      <link rel="canonical" href="https://shanemcfadden.dev" />
-      <meta property="og:title" content="Shane McFadden - Software Developer" />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="canonical" href={url} />
+      <meta property="og:title" content={title} />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://shanemcfadden.dev/" />
-      <meta property="og:description" content={PAGE_DESCRIPTION} />
-      <meta
-        property="og:image"
-        content="https://shanemcfadden.dev/og-image.jpg"
-      />
-      <meta
-        property="twitter:image"
-        content="https://shanemcfadden.dev/twitter-image.jpg"
-      />
+      <meta property="og:url" content={url} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={ogImage} />
+      <meta property="twitter:image" content={twitterImage} />
       <script type="application/ld+json">
         {`{
               "@context": "https://schema.org",
               "@type": "Person",
               "name": "Shane McFadden",
               "jobTitle": "Software Developer",
-              "url": "https://shanemcfadden.dev"
+              "url": "${url}"
             }`}
       </script>
     </Helmet>
@@ -35,3 +31,17 @@ const SEO = () => {
 };
 
 export default SEO;
+
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        description
+        ogImage
+        title
+        twitterImage
+        url
+      }
+    }
+  }
+`;
