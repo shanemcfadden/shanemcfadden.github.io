461aec5280b9ddbdb45518e843a9c167006839b3
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-06-03 10:02:06 -0500

    Navigate to content by default when clicking Project page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,20 +2,24 @@ import { Link } from "gatsby";
 import React from "react";
 
 const Navigation = () => {
+  const isActiveWithHash = ({ location, href }) => {
+    return location.pathname === href.split("#")[0]
+      ? { className: "nav__button nav__button--selected" }
+      : { className: "nav__button" };
+  };
+
   const links = [
-    { to: "/", text: "Projects" },
+    { to: "/#navigation", text: "Projects" },
     { to: "/about", text: "About Me" },
     { to: "/resume", text: "Resume" },
   ];
   return (
-    <nav className="flex-columns flex--justify-content-space-between">
+    <nav
+      id="navigation"
+      className="flex-columns flex--justify-content-space-between"
+    >
       {links.map(({ to, text }) => (
-        <Link
-          to={to}
-          key={to}
-          className="nav__button"
-          activeClassName="nav__button--selected"
-        >
+        <Link to={to} key={to} getProps={isActiveWithHash}>
           {text}
         </Link>
       ))}
