69a234610e005e6a71944c86f2e14f5f284317b4
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-06-02 22:58:28 -0500

    Remove extra comment

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,38 +69,6 @@ const IndexPage = ({ data }) => {
   return (
     <>
       <SEO />
-      {
-        // <Helmet>
-        //   <html lang="en" />
-        //   <title>Shane McFadden - Software Developer</title>
-        //   <meta name="description" content={PAGE_DESCRIPTION} />
-        //   <link rel="canonical" href="https://shanemcfadden.dev" />
-        //   <meta
-        //     property="og:title"
-        //     content="Shane McFadden - Software Developer"
-        //   />
-        //   <meta property="og:type" content="website" />
-        //   <meta property="og:url" content="https://shanemcfadden.dev/" />
-        //   <meta property="og:description" content={PAGE_DESCRIPTION} />
-        //   <meta
-        //     property="og:image"
-        //     content="https://shanemcfadden.dev/og-image.jpg"
-        //   />
-        //   <meta
-        //     property="twitter:image"
-        //     content="https://shanemcfadden.dev/twitter-image.jpg"
-        //   />
-        //   <script type="application/ld+json">
-        //     {`{
-        //         "@context": "https://schema.org",
-        //         "@type": "Person",
-        //         "name": "Shane McFadden",
-        //         "jobTitle": "Software Developer",
-        //         "url": "https://shanemcfadden.dev"
-        //       }`}
-        //   </script>
-        // </Helmet>
-      }
       <div className="banner">
         <div className="content-container">
           <div>Hi, my name is</div>
