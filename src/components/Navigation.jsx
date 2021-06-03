4a98eb314f0324b1ed1cf4e4a797e8ef56468311
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-06-03 09:40:33 -0500

    Refactor

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,29 +2,22 @@ import { Link } from "gatsby";
 import React from "react";
 
 const Navigation = () => {
+  const links = [
+    { to: "/", text: "Projects" },
+    { to: "/about", text: "About Me" },
+    { to: "/resume", text: "Resume" },
+  ];
   return (
     <nav className="flex-columns flex--justify-content-space-between">
-      <Link
-        to="/"
-        className="nav__button"
-        activeClassName="nav__button--selected"
-      >
-        Projects
-      </Link>
-      <Link
-        to="/about"
-        className="nav__button"
-        activeClassName="nav__button--selected"
-      >
-        About Me
-      </Link>
-      <Link
-        to="/resume"
-        className="nav__button"
-        activeClassName="nav__button--selected"
-      >
-        Resume
-      </Link>
+      {links.map(({ to, text }) => (
+        <Link
+          to={to}
+          className="nav__button"
+          activeClassName="nav__button--selected"
+        >
+          {text}
+        </Link>
+      ))}
     </nav>
   );
 };
