31c87b5a8d706b1f84025eb11648e775bb7dceaa
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-06-04 16:57:32 -0500

    Use full url for image meta tags Remove square twitter image

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,6 @@ module.exports = {
     description:
       "Portfolio site for Shane McFadden, a software developer dedicated to building full-stack applications with React, Javascript, Typescript, NodeJS, MySQL, and more.",
     siteUrl: "https://shanemcfadden.dev",
-    twitterImage: "/twitter-image.jpg",
-    ogImage: "/og-image.jpg",
   },
   plugins: [
     "gatsby-plugin-sass",
--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -5,16 +5,10 @@ import Helmet from "react-helmet";
 
 const SEO = () => {
   const { site } = useStaticQuery(query);
-  const {
-    author,
-    description,
-    ogImage,
-    title,
-    twitterImage,
-    siteUrl,
-  } = site.siteMetadata;
+  const { author, description, title, siteUrl } = site.siteMetadata;
   const { pathname } = useLocation();
   const url = siteUrl + pathname;
+  const ogImageUrl = siteUrl + "/og-image.jpg";
   return (
     <Helmet>
       <html lang="en" />
@@ -24,16 +18,16 @@ const SEO = () => {
       <meta name="author" content={author} />
       <meta itemProp="name" content={title} />
       <meta itemProp="description" content={description} />
-      <meta itemProp="image" content={ogImage} />
+      <meta itemProp="image" content={ogImageUrl} />
       <meta property="og:title" content={title} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={ogImage} />
+      <meta property="og:image" content={ogImageUrl} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta property="twitter:image" content={twitterImage} />
+      <meta property="twitter:image" content={ogImageUrl} />
       <script type="application/ld+json">
         {`{
               "@context": "https://schema.org",
@@ -55,9 +49,7 @@ const query = graphql`
       siteMetadata {
         author
         description
-        ogImage
         title
-        twitterImage
         siteUrl
       }
     }
