d5cae3afc46871cde553368b865e833ab16f56ee
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-08-04 13:28:56 -0500

    Remove extra PageDescription file

--- src/content/PageDescription.js
+++
@@ -1,4 +0,0 @@
-const PAGE_DESCRIPTION =
-  "Portfolio site for Shane McFadden, a software developer dedicated to building full-stack applications with React, Javascript, Typescript, NodeJS, MySQL, and more.";
-
-export default PAGE_DESCRIPTION;
