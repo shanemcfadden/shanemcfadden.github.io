2d03540165a86833caef369630d75e4a81941d20
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-08-04 12:58:59 -0500

    Remove gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
-    "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sitemap",
